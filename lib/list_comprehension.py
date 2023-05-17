ed2902ecd0bb683636204b8089fb7bc446280722
Marvin-Kabugi <kabugijeff@gmail.com>
2023-05-17 11:42:07 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+   return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list if sentence_list]
+
+# print(return_evens(range(1,10, 2)))
